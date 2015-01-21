abddc13a0769b9aa9c085067afe12fb6c1c06a50
JRH2794 <JRH2794@email.vccs.edu>
2015-01-21 17:02:18 -0500

    initial commit

---
+++ Unit7/src/com/company/HelloName.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class HelloName {
+
+   public static void main(String[] args) {
+
+       String name;
+
+       System.out.println("Please tell me your username: ");
+       Scanner keyboard = new Scanner(System.in);
+       name = keyboard.next();
+       System.out.println("Please tell me your password: ");
+       System.out.println("Hello " + name + ", Welcome to CSC200 class! and Your password is " + keyboard.next() + "!");
+
+   System.exit(0); } }
